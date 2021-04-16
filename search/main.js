baf2229d9e92f2c5189613b23e0c4fd0f56546bc
CatYoutuber <cat158196@gmail.com>
2021-04-16 17:51:54 +0300

    Beta Review

--- search/main.js
+++
@@ -1,7 +0,0 @@
-var text = document.getElementById("SearchText");
-function search() {
-    window.location = ("catyoutuber.github.io/search?q=" + encodeURI(text.value));
-}
-function load() {
-    text.value = window.location.search.split("?q=")[1];
-}
